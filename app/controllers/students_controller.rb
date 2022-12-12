b6c4d7f8b52e2de4122cfb8f226a9af8467d4a1a
Maxim <polyakovmaxim05@gmail.com>
2022-12-12 16:32:17 -0500

    commit tree 49ffcc925845eca0e6092ce7e1ba4696110aba85

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,14 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def query
+    student = Student.find_by(first_name:params[:key1], last_name:params[:key2])
+    render json: student
+  end
+
 end
